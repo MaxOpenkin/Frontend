0827e55b9daebb53a76acfbc3c4391e7a6395fa5
openk <openkin.max@gmail.com>
2024-05-02 01:02:30 +0600

    lesson_23

--- homeworks/homework_22_fetch_API/index.js
+++ homeworks/homework_22_fetch_API/index.js
@@ -1,25 +1,68 @@
+// document.getElementById('getFact').addEventListener('click', function () {
+//     const button = this;
+//     button.disabled = true;
+//     const factContainer = document.getElementById('fact');
+//     factContainer.innerHTML = 'Loading...';
+//     factContainer.className = '';
+
+//     fetch('https://catfact.ninja/fact')
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network error: ' + response.status);
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             factContainer.innerHTML = data.fact;
+//         })
+//         .catch(error => {
+//             factContainer.innerHTML = 'Error when receiving a fact: ' + error;
+//             factContainer.className = 'error';
+//         })
+//         .finally(() => {
+//             button.disabled = false;
+//         });
+// });
+
+
+
+
+
+
+
 document.getElementById('getFact').addEventListener('click', function () {
     const button = this;
     button.disabled = true;
     const factContainer = document.getElementById('fact');
-    factContainer.innerHTML = 'Загрузка...';
+    factContainer.innerHTML = 'Loading...';
     factContainer.className = '';
 
-    fetch('https://catfact.ninja/fact')
-        .then(response => {
+    // Выполняем два запроса к API
+    Promise.all([
+        fetch('https://catfact.ninja/fact'),
+        fetch('https://catfact.ninja/fact')
+    ])
+    .then(responses => {
+        // Проверяем, что оба ответа в порядке
+        responses.forEach(response => {
             if (!response.ok) {
-                throw new Error('Сетевая ошибка: ' + response.status);
+                throw new Error('Network error: ' + response.status);
             }
-            return response.json();
-        })
-        .then(data => {
-            factContainer.innerHTML = data.fact;
-        })
-        .catch(error => {
-            factContainer.innerHTML = 'Ошибка при получении факта: ' + error;
-            factContainer.className = 'error';
-        })
-        .finally(() => {
-            button.disabled = false;
         });
+        // Преобразуем оба ответа в JSON
+        return Promise.all(responses.map(response => response.json()));
+    })
+    .then(data => {
+        // Обновляем содержимое контейнера фактами
+        factContainer.innerHTML = data[0].fact + "<br><br>" + data[1].fact;
+    })
+    .catch(error => {
+        // Обработка ошибок
+        factContainer.innerHTML = 'Error when receiving a fact: ' + error;
+        factContainer.className = 'error';
+    })
+    .finally(() => {
+        // Восстанавливаем кнопку в исходное состояние
+        button.disabled = false;
+    });
 });
